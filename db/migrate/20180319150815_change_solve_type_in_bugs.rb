47b47efd1fb66fe922a0d254a9fc370fe0d12100
Mariana <mrns.olv@gmail.com>
2018-03-20 01:38:55 -0300

    Changed migration convert type to boolean

--- db/migrate/20180319150815_change_solve_type_in_bugs.rb
+++ db/migrate/20180319150815_change_solve_type_in_bugs.rb
@@ -1,5 +1,5 @@
 class ChangeSolveTypeInBugs < ActiveRecord::Migration[5.0]
   def change
-  	change_column :bugs, :solve, :boolean
+  	change_column :bugs, :solve, 'boolean USING CAST(solve AS boolean)'
   end
 end
