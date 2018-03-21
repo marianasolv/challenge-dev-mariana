e44598ff244a6cacec2c57538d7c9dee48627840
Mariana <mrns.olv@gmail.com>
2018-03-20 23:59:25 -0300

    Added the foreign key User Bugs

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180321025529) do
+ActiveRecord::Schema.define(version: 20180321025753) do
 
   create_table "bugs", force: :cascade do |t|
     t.string   "name"
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(version: 20180321025529) do
     t.boolean  "solve"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "user_id"
+    t.index ["user_id"], name: "index_bugs_on_user_id"
   end
 
   create_table "projects", force: :cascade do |t|
