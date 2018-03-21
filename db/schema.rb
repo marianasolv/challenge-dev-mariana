070933848455c068688caa826b60db1696b03bb0
Mariana <mrns.olv@gmail.com>
2018-03-20 23:55:16 -0300

    Added foreign key Project User

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180321021809) do
+ActiveRecord::Schema.define(version: 20180321025418) do
 
   create_table "projects", force: :cascade do |t|
     t.string   "name"
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(version: 20180321021809) do
     t.date     "start_date"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "user_id"
+    t.index ["user_id"], name: "index_projects_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
